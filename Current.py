686fe8675b62af61ec165cbdedd2f01e99c3ef1b
OBELETUMAZIBE <tumazibeobele@gmail.com>
2025-06-17 12:50:42 +0100

    Create Current.py
    Fabunmi made the current code

---
+++ Current.py
@@ -0,0 +1,10 @@
+# Current Account with no limit
+class CurrentAccount(Account):
+    def __init__(self, name):
+        super().__init__(name, "current")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            return f"{amount} withdrawn from current account."
+        return "Insufficient funds."
